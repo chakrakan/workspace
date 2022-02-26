232d784789240467936972eb728e7bb174bff29a
Kanisk Chakraborty <57884127+chakrakan@users.noreply.github.com>
2022-02-26 13:00:14 -0500

    WIP: remove 'jit' mode since it's default in v3

--- apps/site/tailwind.config.js
+++ apps/site/tailwind.config.js
@@ -1,7 +1,6 @@
 const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
 const { join } = require('path');
 module.exports = {
-  mode: 'jit',
   presets: [require('../../tailwind-workspace-preset.js')],
   content: [
     join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
--- libs/shared/ui/tailwind.config.js
+++ libs/shared/ui/tailwind.config.js
@@ -2,7 +2,6 @@ const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
 const { join } = require('path');
 
 module.exports = {
-  mode: 'jit',
   presets: [require('../../../tailwind-workspace-preset.js')],
   content: [
     join(__dirname, '../src/**/*.{js,ts,jsx,tsx}'),
